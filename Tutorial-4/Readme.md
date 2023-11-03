815b491e787fbcaebdf11b9aab463b6fb06e4570
Saisantoshkumar Tuniki <saisantosh.tuniki@onpasive.com>
2023-11-03 09:27:07 +0530

    boolean dataType adde

---
+++ Tutorial-4/Readme.md
@@ -0,0 +1,7 @@
+###  boolean ( True | False )
+
+### Unlike other programming languauges, boolean in Python is Capitalized.
+
+### The 'type' for boolean class is "bool"
+
+    print(type(True))   #   <class 'bool'>   
---
+++ Tutorial-4/boolean.py
@@ -0,0 +1,15 @@
+# BOOLEAN
+
+# LITERAL ASSIGNMENT
+myValue = True
+
+print(type(myValue))                #   <class 'bool'>
+print(isinstance(myValue, bool))    # True
+print('')
+
+# CONSTRUCTOR FUNCTION
+isTrue = bool(False)
+
+print(type(isTrue))                 #   <class 'bool'>
+print(isinstance(isTrue, bool))     #   True
+print('')
